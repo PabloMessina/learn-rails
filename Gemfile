9bf7e87df5711dc6a8e1236be9bff6a5c92f22fa
Pablo Messina <pamessina@uc.cl>
2013-12-19 21:52:27 -0300

    mailing list

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'google_drive'
 gem 'high_voltage'
 gem 'simple_form'
 gem 'zurb-foundation'
+gem 'certified'
 group :development do
 	gem 'better_errors'
 	gem 'quiet_assets'
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,9 @@ class ContactsController < ApplicationController
 	def create
 		@contact = Contact.new(secure_params)
 		if @contact.valid?
-			# TODO save data
-			# TODO send message
-			flash[:notice] = "Message sent from #{@contact.name}"
+			@contact.update_spreadsheet
+			UserMailer.contact_email(@contact).deliver
+			flash[:notice] = "Message sent from #{@contact.name}."
 			redirect_to root_path
 		else
 			render :new
